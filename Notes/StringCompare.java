428a9d318d83580139ee4a73b471d3378a6c0d74
hloh7967 <harrison3219@yahoo.com>
2016-10-17 11:25:06 -0500

    lol

---
+++ Notes/StringCompare.java
@@ -0,0 +1,25 @@
+
+
+/**
+ * Write a description of class StringCompare here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class StringCompare
+{
+    public static void main (String[] args)
+    {
+        String word1 = "catalog";
+        String word2 = "cat";
+        word1.compareTo("aaa");
+        word1.compareTo(word2);
+        word1.compareTo(word2);
+        String word1pre = word1.substring(0,2);
+        String word2pre = word2.substring(0,2);
+        word1pre.compareTo(word2pre);
+        
+    }
+
+
+}
